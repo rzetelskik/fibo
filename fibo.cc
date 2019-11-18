419711cdc5fc2d9d00c509120f1a787fc63468aa
Kacper Rzetelski <kr406321@students.mimuw.edu.pl>
2019-11-18 23:07:27 +0100

    implemented bitwise operators

--- fibo.cc
+++ fibo.cc
@@ -1,11 +1,17 @@
-//TODO do wyjebania
-#include <iostream>
+#include <functional>
 #include "fibo.h"
 
 Fibo::Fibo(const std::string& str) {
-    //TODO sanity check?
     this->bits = boost::dynamic_bitset<>(str);
-    //TODO normalize
+    normalise();
+}
+
+void Fibo::normalise() {
+    //TODO
+}
+
+Fibo::Fibo(unsigned long n) {
+    //TODO
 }
 
 Fibo& Fibo::operator=(const Fibo& other) {
@@ -15,9 +21,18 @@ Fibo& Fibo::operator=(const Fibo& other) {
     return *this;
 }
 
-Fibo::Fibo(unsigned long n) {
-    //TODO
+Fibo& Fibo::performBitwiseOperation(const Fibo& other, const BitFunction& f) {
+    size_t min = std::min(this->length(), other.length());
+    for (size_t i = 0; i < min; i++) {
+        this->bits[i] = f(this->bits[i], other.bits[i]);
+    }
+    for (size_t i = min; i < other.length(); i++) {
+        this->bits.push_back(f(other.bits[i], false));
+    }
+
+    return *this;
 }
+
 
 Fibo& Fibo::operator+=(const Fibo& other) {
     //TODO
@@ -25,51 +40,85 @@ Fibo& Fibo::operator+=(const Fibo& other) {
 }
 
 Fibo& Fibo::operator&=(const Fibo& other) {
-    //TODO
-    return *this;
+    return performBitwiseOperation(other, std::bit_and<>());
 }
 
 Fibo& Fibo::operator|=(const Fibo& other) {
-    //TODO
-    return *this;
+    return performBitwiseOperation(other, std::bit_or<>());
 }
 
 Fibo& Fibo::operator^=(const Fibo &other) {
-    //TODO
-    return *this;
+    return performBitwiseOperation(other, std::bit_xor<>());
 };
 
 Fibo& Fibo::operator<<=(unsigned long n) {
-    //TODO
+    //TODO exception for n < 0?
+    if (!n) {
+        return *this;
+    }
+
+    this->bits.resize(this->length() + n, false);
+    this->bits <<= n;
     return *this;
 }
 
-const Fibo& Fibo::operator+(const Fibo& other) const {
+const Fibo Fibo::operator+(const Fibo& other) const {
     return Fibo(*this) += other;
 }
 
-const Fibo& Fibo::operator&(const Fibo& other) const {
+const Fibo Fibo::operator&(const Fibo& other) const {
     return Fibo(*this) &= other;
 }
 
-const Fibo& Fibo::operator|(const Fibo& other) const {
+const Fibo Fibo::operator|(const Fibo& other) const {
     return Fibo(*this) |= other;
 }
 
-const Fibo& Fibo::operator^(const Fibo& other) const {
+const Fibo Fibo::operator^(const Fibo& other) const {
     return Fibo(*this) ^= other;
 }
 
-const Fibo& Fibo::operator<<(unsigned long n) const {
+const Fibo Fibo::operator<<(unsigned long n) const {
     return Fibo(*this) <<= n;
-
 }
 
-unsigned long Fibo::length() {
+unsigned long Fibo::length() const {
     return bits.size();
 }
 
 std::ostream& operator<<(std::ostream& os, const Fibo& fibo) {
     os << fibo.bits;
     return os;
+}
+
+bool Fibo::operator==(const Fibo& other) const {
+    return this->bits == other.bits;
+}
+
+bool Fibo::operator<(const Fibo& other) const {
+    return this->bits < other.bits;
+}
+
+bool Fibo::operator<=(const Fibo& other) const {
+    return this->bits <= other.bits;
+}
+
+bool Fibo::operator!=(const Fibo& other) const {
+    return this->bits != other.bits;
+}
+
+bool Fibo::operator>(const Fibo& other) const {
+    return this->bits > other.bits;
+}
+
+bool Fibo::operator>=(const Fibo& other) const {
+    return this->bits >= other.bits;
+}
+
+const Fibo Zero() {
+    return Fibo();
+}
+
+const Fibo One() {
+    return Fibo(1);
 }
--- fibo.h
+++ fibo.h
@@ -3,9 +3,13 @@
 
 #include <boost/dynamic_bitset.hpp>
 
+using BitFunction = std::function<bool(bool, bool)>;
+
 class Fibo {
 private:
     boost::dynamic_bitset<> bits;
+    void normalise();
+    Fibo& performBitwiseOperation(const Fibo& other, const BitFunction& f);
 public:
     // Sets all bits to zeros by default.
     Fibo() : bits(1) {};
@@ -15,24 +19,32 @@ public:
     //TODO int? long long?
     explicit Fibo(unsigned long n);
 
-    //TODO pojecia nie mam czy to jest optymalne, trzeba zerknac na czytanki
-    Fibo& operator=(const Fibo &other);
-    Fibo& operator+=(const Fibo &other);
-    Fibo& operator&=(const Fibo &other);
-    Fibo& operator|=(const Fibo &other);
-    Fibo& operator^=(const Fibo &other);
+    Fibo& operator=(const Fibo& other);
+    Fibo& operator+=(const Fibo& other);
+    Fibo& operator&=(const Fibo& other);
+    Fibo& operator|=(const Fibo& other);
+    Fibo& operator^=(const Fibo& other);
     Fibo& operator<<=(unsigned long n);
-    const Fibo& operator+(const Fibo &other) const;
-    const Fibo& operator&(const Fibo &other) const;
-    const Fibo& operator|(const Fibo &other) const;
-    const Fibo& operator^(const Fibo &other) const;
-    const Fibo& operator<<(unsigned long n) const;
-    friend std::ostream& operator<<(std::ostream &os, const Fibo& fibo);
-    unsigned long length();
+
+    const Fibo operator+(const Fibo& other) const;
+    const Fibo operator&(const Fibo& other) const;
+    const Fibo operator|(const Fibo& other) const;
+    const Fibo operator^(const Fibo& other) const;
+    const Fibo operator<<(unsigned long n) const;
+
+    bool operator==(const Fibo& other) const;
+    bool operator<(const Fibo& other) const;
+    bool operator<=(const Fibo& other) const;
+    bool operator!=(const Fibo& other) const;
+    bool operator>(const Fibo& other) const;
+    bool operator>=(const Fibo& other) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const Fibo& fibo);
+    [[nodiscard]] unsigned long length() const;
 };
 
-Fibo Zero();
+const Fibo Zero();
 
-Fibo One();
+const Fibo One();
 
 #endif //FIBO_FIBO_H
