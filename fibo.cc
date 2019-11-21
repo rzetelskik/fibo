5a45b1e28c9ef69f6d74f3d6bb0fda0b203aeb11
Tomasz Młynarczyk <tomasz.mlynarczyk@codilime.com>
2019-11-21 15:44:29 +0100

    Run clang-format

--- fibo.cc
+++ fibo.cc
@@ -1,222 +1,224 @@
 #include "fibo.h"
-#include <vector>
 #include <iostream>
+#include <vector>
 
 namespace {
-    std::vector<long long>& fibonacciVector() {
-        static size_t maxFit = 91;
-        static std::vector<long long> fibonacciVector(maxFit, 0);
-        return fibonacciVector;
-    }
-
-    void fibonacciComputeIfAbsent(size_t pos) {
-        if (fibonacciVector().size() <= pos) {
-            //TODO throw exception instead?
-            fibonacciVector().resize(pos, 0);
-        }
-        if (fibonacciVector()[pos]) return;
-
-        fibonacciVector()[pos] = ((pos > 0) ? fibonacciVector()[pos - 1] : 0)
-                                 + ((pos > 1) ? fibonacciVector()[pos - 2] : 1);
-    }
-
-    size_t fibonacciGetBestFit(long long n) {
-        size_t pos = 0;
-        fibonacciComputeIfAbsent(pos);
-
-        do {
-            fibonacciComputeIfAbsent(pos);
-        } while (fibonacciVector()[pos++] <= n);
-
-        return --pos;
-    }
-
-    bool isStringValid(const std::string& str) {
-        //TODO leading zeros!
-        for (auto c: str) {
-            if ('1' < c || c < '0') return false;
-        }
-
-        return true;
-    }
+std::vector<long long> &fibonacciVector() {
+  static size_t maxFit = 91;
+  static std::vector<long long> fibonacciVector(maxFit, 0);
+  return fibonacciVector;
 }
 
-Fibo::Fibo(const std::string& str) {
-    if (!isStringValid(str)) throw std::invalid_argument("Invalid argument provided.");
-    this->bits = boost::dynamic_bitset<>(str);
-    normaliseBits();
+void fibonacciComputeIfAbsent(size_t pos) {
+  if (fibonacciVector().size() <= pos) {
+    // TODO throw exception instead?
+    fibonacciVector().resize(pos, 0);
+  }
+  if (fibonacciVector()[pos])
+    return;
+
+  fibonacciVector()[pos] = ((pos > 0) ? fibonacciVector()[pos - 1] : 0) +
+                           ((pos > 1) ? fibonacciVector()[pos - 2] : 1);
+}
+
+size_t fibonacciGetBestFit(long long n) {
+  size_t pos = 0;
+  fibonacciComputeIfAbsent(pos);
+
+  do {
+    fibonacciComputeIfAbsent(pos);
+  } while (fibonacciVector()[pos++] <= n);
+
+  return --pos;
+}
+
+bool isStringValid(const std::string &str) {
+  // TODO leading zeros!
+  for (auto c : str) {
+    if ('1' < c || c < '0')
+      return false;
+  }
+
+  return true;
+}
+} // namespace
+
+Fibo::Fibo(const std::string &str) {
+  if (!isStringValid(str))
+    throw std::invalid_argument("Invalid argument provided.");
+  this->bits = boost::dynamic_bitset<>(str);
+  normaliseBits();
 }
 
 Fibo::Fibo(long long n) {
-    if (n < 0) throw std::invalid_argument("Negative value provided.");
-    if (n == 0) {
-        *this = Fibo();
-        return;
+  if (n < 0)
+    throw std::invalid_argument("Negative value provided.");
+  if (n == 0) {
+    *this = Fibo();
+    return;
+  }
+
+  size_t maxPos = fibonacciGetBestFit(n);
+  bits.resize(maxPos, false);
+
+  long long fib = n;
+  for (size_t i = maxPos; i <= maxPos && fib > 0; i--) {
+    fib = fibonacciVector()[i];
+
+    if (fib <= n) {
+      n -= fib;
+      bits.set(i, true);
     }
-
-    size_t maxPos = fibonacciGetBestFit(n);
-    bits.resize(maxPos, false);
-
-    long long fib = n;
-    for (size_t i = maxPos; i <= maxPos && fib > 0; i--) {
-        fib = fibonacciVector()[i];
-
-        if (fib <= n) {
-            n -= fib;
-            bits.set(i, true);
-        }
-    }
+  }
 }
 
-
 void Fibo::clearLeadingZeroBits() {
-    size_t i = this->length() - 1;
-    while (i > 0 && !this->bits[i]) --i;
+  size_t i = this->length() - 1;
+  while (i > 0 && !this->bits[i])
+    --i;
 
-    this->bits.resize(i + 1);
+  this->bits.resize(i + 1);
 }
 
 void Fibo::clearBitsInRange(size_t begin, size_t end) {
-    if (end < begin) std::swap(begin, end);
-    for (size_t i = begin; i <= end; i++) {
-        this->bits[i] = false;
-    }
+  if (end < begin)
+    std::swap(begin, end);
+  for (size_t i = begin; i <= end; i++) {
+    this->bits[i] = false;
+  }
 }
 
 void Fibo::normaliseBits() {
-    clearLeadingZeroBits();
+  clearLeadingZeroBits();
 
-    this->bits.push_back(false);
-    size_t memZero = this->length() - 1, lowestSetBit = this->bits.find_first();
+  this->bits.push_back(false);
+  size_t memZero = this->length() - 1, lowestSetBit = this->bits.find_first();
 
-    for (size_t i = this->length() - 2; i > lowestSetBit; i--) {
-        if (!this->bits[i] && !this->bits[i - 1]) {
-            memZero = i - 1;
-        } else if (this->bits[i] && this->bits[i - 1]) {
-            this->bits[memZero] = true;
-            clearBitsInRange(memZero - 1, i - 1);
-            memZero = i - 1;
-        }
+  for (size_t i = this->length() - 2; i > lowestSetBit; i--) {
+    if (!this->bits[i] && !this->bits[i - 1]) {
+      memZero = i - 1;
+    } else if (this->bits[i] && this->bits[i - 1]) {
+      this->bits[memZero] = true;
+      clearBitsInRange(memZero - 1, i - 1);
+      memZero = i - 1;
     }
+  }
 
-    clearLeadingZeroBits();
+  clearLeadingZeroBits();
 }
 
-Fibo& Fibo::operator=(const Fibo& other) {
-    if (this != &other) {
-        bits = other.bits;
-    }
-    return *this;
+Fibo &Fibo::operator=(const Fibo &other) {
+  if (this != &other) {
+    bits = other.bits;
+  }
+  return *this;
 }
 
 bool Fibo::getOrDefault(size_t i, bool value) const {
-    if (this->length() > i) {
-        return this->bits[i];
+  if (this->length() > i) {
+    return this->bits[i];
+  }
+  return value;
+}
+
+Fibo &Fibo::performBitwiseOperation(const Fibo &other, const BitFunction &f) {
+  size_t min = this->length(), max = other.length();
+  if (max < min)
+    std::swap(min, max);
+
+  for (size_t i = 0; i < min; i++) {
+    bits.set(i, f(this->bits[i], other.bits[i]));
+  }
+
+  if (this->length() < max)
+    this->bits.resize(max, false);
+  for (size_t i = min; i < max; i++) {
+    bits.set(i, f(this->bits[i], other.getOrDefault(i, false)));
+  }
+
+  normaliseBits();
+  return *this;
+}
+
+Fibo &Fibo::operator+=(const Fibo &other) {
+  size_t len1 = length();
+  size_t len2 = other.length();
+  if (len1 < len2)
+    bits.resize(len2, false);
+  else if (len1 == len2)
+    bits.resize(len1 + 1, false);
+  bits.push_back(false);
+
+  bool rem[3] = {false, false, false};
+  int j = 0;
+  for (size_t i = len2; i-- > 0;) {
+    if (!other.bits.test(i) && !rem[j]) {
+    } else if ((other.bits.test(i) && rem[j]) || bits.test(i)) {
+      if (bits.test(i + 1)) {
+        bits.set(i + 2, true);
+        rem[(j + 1) % 3] = true;
+      }
+      bits.flip(i + 1);
+      rem[(j + 2) % 3] = true;
+      if (!other.bits.test(i) || !rem[j])
+        bits.set(i, false);
+    } else {
+      bits.set(i, true);
     }
-    return value;
-}
-
-Fibo& Fibo::performBitwiseOperation(const Fibo& other, const BitFunction& f) {
-    size_t min = this->length(), max = other.length();
-    if (max < min) std::swap(min, max);
-
-    for (size_t i = 0; i < min; i++) {
-        bits.set(i, f(this->bits[i], other.bits[i]));
+    if (bits.test(i) && bits.test(i + 1)) {
+      bits.flip(i);
+      bits.flip(i + 1);
+      bits.flip(i + 2);
     }
+    rem[j] = false;
+    j = (j + 1) % 3;
+  }
+  if (rem[j] && bits.test(0)) {
+    bits.flip(0);
+    bits.set(1, true);
+  } else if (rem[j])
+    bits.set(0, true);
 
-    if (this->length() < max) this->bits.resize(max, false);
-    for (size_t i = min; i < max; i++) {
-        bits.set(i, f(this->bits[i], other.getOrDefault(i, false)));
-    }
-
-    normaliseBits();
-    return *this;
+  normaliseBits();
+  return *this;
 }
 
-Fibo& Fibo::operator+=(const Fibo& other) {
-    size_t len1 = length();
-    size_t len2 = other.length();
-    if (len1 < len2)
-        bits.resize(len2, false);
-    else if (len1 == len2)
-        bits.resize(len1 + 1, false);
-    bits.push_back(false);
-
-    bool rem[3] = {false, false, false};
-    int j = 0;
-    for (size_t i = len2; i --> 0;) {
-        if (!other.bits.test(i) && !rem[j]) {}
-        else if ((other.bits.test(i) && rem[j]) || bits.test(i)) {
-            if (bits.test(i + 1)) {
-                bits.set(i + 2, true);
-                rem[(j + 1) % 3] = true;
-            }
-            bits.flip(i + 1);
-            rem[(j + 2) % 3] = true;
-            if (!other.bits.test(i) || !rem[j])
-                bits.set(i, false);
-        } else {
-            bits.set(i, true);
-        }
-        if (bits.test(i) && bits.test(i + 1)) {
-            bits.flip(i);
-            bits.flip(i + 1);
-            bits.flip(i + 2);
-        }
-        rem[j] = false;
-        j = (j + 1) % 3;
-    }
-    if (rem[j] && bits.test(0)) {
-        bits.flip(0);
-        bits.set(1, true);
-    } else if (rem[j])
-        bits.set(0, true);
-
-    normaliseBits();
-    return *this;
+Fibo &Fibo::operator&=(const Fibo &other) {
+  return performBitwiseOperation(other, std::bit_and<>());
 }
 
-Fibo& Fibo::operator&=(const Fibo& other) {
-    return performBitwiseOperation(other, std::bit_and<>());
+Fibo &Fibo::operator|=(const Fibo &other) {
+  return performBitwiseOperation(other, std::bit_or<>());
 }
 
-Fibo& Fibo::operator|=(const Fibo& other) {
-    return performBitwiseOperation(other, std::bit_or<>());
-}
-
-Fibo& Fibo::operator^=(const Fibo &other) {
-    return performBitwiseOperation(other, std::bit_xor<>());
+Fibo &Fibo::operator^=(const Fibo &other) {
+  return performBitwiseOperation(other, std::bit_xor<>());
 };
 
-Fibo& Fibo::operator<<=(size_t n) {
-    if (!n) return *this;
-
-    this->bits.resize(this->length() + n, false);
-    this->bits <<= n;
+Fibo &Fibo::operator<<=(size_t n) {
+  if (!n)
     return *this;
+
+  this->bits.resize(this->length() + n, false);
+  this->bits <<= n;
+  return *this;
 }
 
-bool operator==(const Fibo& left, const Fibo& right) {
-    return left.bits == right.bits;
+bool operator==(const Fibo &left, const Fibo &right) {
+  return left.bits == right.bits;
 }
 
-bool operator<(const Fibo& left, const Fibo& right) {
-    return left.bits < right.bits;
+bool operator<(const Fibo &left, const Fibo &right) {
+  return left.bits < right.bits;
 }
 
-std::ostream& operator<<(std::ostream& os, const Fibo& fibo) {
-    os << fibo.bits;
-    return os;
+std::ostream &operator<<(std::ostream &os, const Fibo &fibo) {
+  os << fibo.bits;
+  return os;
 }
 
-const Fibo Zero() {
-    return Fibo();
-}
+const Fibo Zero() { return Fibo(); }
 
-const Fibo One() {
-    return Fibo(1);
-}
+const Fibo One() { return Fibo(1); }
 
-size_t Fibo::length() const {
-    return bits.size();
-}
+size_t Fibo::length() const { return bits.size(); }
--- fibo.h
+++ fibo.h
@@ -7,43 +7,48 @@
 
 using BitFunction = std::function<bool(bool, bool)>;
 
-
-class Fibo : boost::addable<Fibo>, boost::bitwise<Fibo>,
-        boost::left_shiftable<Fibo, size_t>, boost::totally_ordered<Fibo> {
+class Fibo : boost::addable<Fibo>,
+             boost::bitwise<Fibo>,
+             boost::left_shiftable<Fibo, size_t>,
+             boost::totally_ordered<Fibo> {
 public:
-    Fibo() : bits(1, false) {};
-    Fibo(const Fibo& other) = default;
-    Fibo(Fibo&& other) noexcept : bits(std::move(other.bits)) {}
-    ~Fibo() = default;
-    explicit Fibo(const std::string& str);
-    Fibo(long long n);
-    template <typename T, typename = typename std::enable_if<(std::is_same<T, bool>::value
-            || std::is_same<T, char>::value || std::is_same<T, wchar_t>::value
-            || std::is_same<T, char16_t>::value || std::is_same<T, char32_t>::value)>::type>
-    Fibo (T t) = delete;
-    Fibo& operator=(const Fibo& other);
-    Fibo& operator+=(const Fibo& other);
-    Fibo& operator&=(const Fibo& other);
-    Fibo& operator|=(const Fibo& other);
-    Fibo& operator^=(const Fibo& other);
-    Fibo& operator<<=(size_t n);
+  Fibo() : bits(1, false){};
+  Fibo(const Fibo &other) = default;
+  Fibo(Fibo &&other) noexcept : bits(std::move(other.bits)) {}
+  ~Fibo() = default;
+  explicit Fibo(const std::string &str);
+  Fibo(long long n);
+  template <
+      typename T,
+      typename = typename std::enable_if<(
+          std::is_same<T, bool>::value || std::is_same<T, char>::value ||
+          std::is_same<T, wchar_t>::value || std::is_same<T, char16_t>::value ||
+          std::is_same<T, char32_t>::value)>::type>
+  Fibo(T t) = delete;
+  Fibo &operator=(const Fibo &other);
+  Fibo &operator+=(const Fibo &other);
+  Fibo &operator&=(const Fibo &other);
+  Fibo &operator|=(const Fibo &other);
+  Fibo &operator^=(const Fibo &other);
+  Fibo &operator<<=(size_t n);
 
-    friend bool operator==(const Fibo& left, const Fibo& right);
-    friend bool operator<(const Fibo& left, const Fibo& right);
-    friend std::ostream& operator<<(std::ostream& os, const Fibo& fibo);
+  friend bool operator==(const Fibo &left, const Fibo &right);
+  friend bool operator<(const Fibo &left, const Fibo &right);
+  friend std::ostream &operator<<(std::ostream &os, const Fibo &fibo);
 
-    [[nodiscard]] size_t length() const;
+  [[nodiscard]] size_t length() const;
+
 private:
-    boost::dynamic_bitset<> bits;
-    void clearBitsInRange(size_t begin, size_t end);
-    void clearLeadingZeroBits();
-    void normaliseBits();
-    [[nodiscard]] bool getOrDefault(size_t i, bool value) const;
-    Fibo& performBitwiseOperation(const Fibo& other, const BitFunction& f);
+  boost::dynamic_bitset<> bits;
+  void clearBitsInRange(size_t begin, size_t end);
+  void clearLeadingZeroBits();
+  void normaliseBits();
+  [[nodiscard]] bool getOrDefault(size_t i, bool value) const;
+  Fibo &performBitwiseOperation(const Fibo &other, const BitFunction &f);
 };
 
 const Fibo Zero();
 
 const Fibo One();
 
-#endif //FIBO_FIBO_H
+#endif // FIBO_FIBO_H
